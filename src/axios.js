57e1a01299e5d456ad0fc9f7e6d951feaa16f653
Vivek K J <58988126+vivekkj123@users.noreply.github.com>
2022-09-05 21:28:53 +0530

    Migrate from heroku to @deta

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const instance = axios.create({
   // THE API (cloud function) URL
-  baseURL: 'https://salty-brushlands-39654.herokuapp.com/'
+//   baseURL: 'https://salty-brushlands-39654.herokuapp.com/'
+  // migrate from heroku to @Deta
+  baseURL: 'https://amazon-clone-backend.deta.dev/'
     // "http://localhost:5001/challenge-4b2b2/us-central1/api",
 });
 
